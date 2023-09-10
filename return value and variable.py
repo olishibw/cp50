2811936b8d7e0dd7aebc299fdcffa305b9221700
pc <olishib2@gmail.com>
2023-09-10 18:26:32 +0300

    initial commit

--- return value and variable.py
+++ return value and variable.py
@@ -1,4 +1,14 @@
 # ask the name of the user
-name=input("what's your name?")
+name=input("what's your name? ").strip().title()
+# removing space
+#name = name.strip()
+# capitalize the later the first name first letter
+#name = name.capitalize()
+# capitalize the first letter the text
+#name = name.title()
+# to capitalize and removing white space
+#name = name.strip().title()
+first , last = name.split(" ")
+
 # say hello to the user
-print("hello,",name)
+print("hello,", first)
